261c2eac44e6624a52248cf5912dfe0864e7f676
param-ninthu <ninthu1999@gmail.com>
2023-05-18 23:19:50 +0530

    have to set useEffect

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Container, Form, Input } from "./homeElements";
 
 const Home = () => {
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=8e1dca7d10efdb98ec902b89f9b2d354`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, [location]);
   return (
     <Container $mode="primary">
       <Container $mode="secondary">
-        {" "}
         <Form>
-          <Input type="text" placeholder="Enter the location" />
+          <Input
+            type="text"
+            placeholder="Enter the location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
           <Button type="submit">Search</Button>
-        </Form>{" "}
+        </Form>
       </Container>
     </Container>
   );
