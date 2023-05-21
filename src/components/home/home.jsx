c2b64ea2679d04a15e5c3578ad8a328e0c91733b
param-ninthu <ninthu1999@gmail.com>
2023-05-22 01:10:29 +0530

    have to modify the output

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,18 +3,20 @@ import { Button, Container, Form, Input } from "./homeElements";
 
 const Home = () => {
   const [location, setLocation] = useState("");
-
+  const [data, setData] = useState([]);
   const getWeather = async () => {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPEN_WEATHER_API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=8e1dca7d10efdb98ec902b89f9b2d354`
     );
-    const data = await response.json();
-    console.log(data);
+    const record = await response.json();
+    setData(record);
+    console.log(record);
   };
 
-  if (location) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     getWeather();
-  }
+  };
 
   return (
     <Container $mode="primary">
@@ -25,7 +27,14 @@ const Home = () => {
             placeholder="Enter the location"
             onChange={(e) => setLocation(e.target.value)}
           />
-          <Button type="submit">Search</Button>
+          <Button onClick={handleSubmit}>Search</Button>
+          {data.main && (
+            <div>
+              <h1>{data.name}</h1>
+              <h2>{data.main.temp}</h2>
+              <h2>{data.weather[0].description}</h2>
+            </div>
+          )}
         </Form>
       </Container>
     </Container>
